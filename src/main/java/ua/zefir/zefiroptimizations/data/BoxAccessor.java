e6841214a812316878f877b36157b056c912e05c
Stikulzon <stickulzon@gmail.com>
2025-03-01 03:29:16 +0100

    deleted useless mixins

--- src/main/java/ua/zefir/zefiroptimizations/data/BoxAccessor.java
+++
@@ -1,7 +0,0 @@
-package ua.zefir.zefiroptimizations.data;
-
-import net.minecraft.util.math.Box;
-
-public interface BoxAccessor {
-    Box clone();
-}
--- src/main/java/ua/zefir/zefiroptimizations/data/EntityAccessor.java
+++
@@ -1,7 +0,0 @@
-package ua.zefir.zefiroptimizations.data;
-
-import net.minecraft.entity.Entity;
-
-public interface EntityAccessor {
-    Entity clone();
-}
